8b447a43d2892b2ed51f35cc67f6fb62fffc508a
Vinicius Aranda <vinicius.aranda@outlook.com>
2023-08-30 16:00:50 -0300

    Starting my new portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Pages/Home/index";
+import Projects from "./Pages/Projects/index";
+import History from "./Pages/History/index";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+      <Menu/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/projetos" element={<Projects/>}  />
+          <Route path="/historia" element={<History/>}  />
+        
+         
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
